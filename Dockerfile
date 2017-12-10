fe7bcd616796acb765498d010a5f0f5fcd1ac8bd
Dmitry Rodon <madiedinro@gmail.com>
2017-12-10 14:27:43 +0300

    open version

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM php:7.1-cli
+
+MAINTAINER Dmitry Rodin <madiedinro@gmail.com>
+
+RUN apt-get update \
+  && apt-get install -y libcurl4-openssl-dev libevent-dev libssl-dev git \
+  && rm -rf /var/lib/apt/lists/* \
+  && docker-php-source extract \
+  && docker-php-ext-install sockets \
+  && pecl install event\
+  && pecl install eio \
+  && docker-php-ext-enable sockets event eio \
+  && curl -sS https://getcomposer.org/installer | php \
+  && mv composer.phar /usr/local/bin/composer
+
+ADD composer.json /tmp/composer.json
+RUN cd /tmp && composer install
+RUN mkdir -p /opt/app \
+  && cp -a /tmp/vendor /opt/app/ \
+  && cp /tmp/composer.lock /opt/app/
+
+# From here we load our application's code in, therefore the previous docker
+# "layer" thats been cached will be used if possible
+WORKDIR /opt/app
+ADD . /opt/app
+
+EXPOSE 8080
+
+CMD ["php", "server.php", "--host=0.0.0.0", "--port=8080"]
---
+++ server.php
@@ -0,0 +1,94 @@
+<?php
+
+chdir(__DIR__);
+require_once 'vendor/autoload.php';
+
+use React\Http\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\Server;
+
+$cmd = new Commando\Command();
+
+$cmd->option('p')
+  ->aka('port')
+  ->default(8080)
+  ->describedAs('Listen port');
+
+$cmd->option('f')
+  ->aka('file')
+  ->default('SxGeoCity.dat')
+  ->describedAs('Database file');
+
+echo "Loading device database\n";
+
+// SxGeo class is not a composer package =(
+require 'SxGeo.php';
+
+$loop = React\EventLoop\Factory::create();
+
+if (file_exists($cmd['file']) === false || is_readable($cmd['file']) === false) {
+  echo "Unable to open SypexGEO data file: \"{$cmd['file']}\"\n";
+  exit(1);
+}
+
+try {
+  $SxGeo = new SxGeo($cmd['file'], SXGEO_BATCH | SXGEO_MEMORY);
+} catch (Exception $e) {
+  echo "Unable to create SypexGEO class on data file: \"{$cmd['file']}\"\n";
+  echo "Error: " . $e->getMessage() . "\n";
+  exit(1);
+}
+
+$server = new \React\Http\Server(function (ServerRequestInterface $request) use ($SxGeo) {
+
+  switch ($request->getUri()->getPath()) {
+    case '/query':
+
+      $startTime = microtime(true);
+      $queryParams = $request->getQueryParams();
+      $result = [];
+
+      $ip = isset($queryParams['ip']) ? (string)$queryParams['ip'] : false;
+
+      if ($ip !== false) {
+        try {
+
+          $result = $SxGeo->getCityFull($ip);
+          $result['execution'] = number_format(microtime(true) - $startTime, 12);
+          $result['error'] = false;
+
+
+        } catch (\Exception $e) {
+          $result = [
+            'error' => true,
+            'message' => $e->getMessage(),
+            'line' => $e->getLine(),
+            'file' => $e->getFile(),
+            'code' => $e->getCode(),
+            'execution' => number_format(microtime(true) - $startTime, 12)
+          ];
+        }
+      } else {
+        $result = [
+          'error' => true,
+          'message' => 'Bad arguments',
+        ];
+      }
+
+      return new Response(
+        200,
+        array('Content-Type' => 'application/json'),
+        json_encode($result)
+      );
+    default:
+      return new Response(404);
+  }
+});
+
+$socket = new \React\Socket\Server('0.0.0.0:' . $cmd['port'], $loop);
+$server->listen($socket);
+
+echo "Starting server {$cmd['port']} \n";
+
+$loop->run();
+
